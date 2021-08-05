4089bcc8e063112afc6d274fcd0c07ea01ee8cfe
anilk77 <ak0506493@gmail.com>
2021-08-05 18:15:08 +0530

    updated twilio auth token

--- app/routes/apiUserRoutes.js
+++ app/routes/apiUserRoutes.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 const accountSid = 'ACa9041a924682d1d3043adada8420ec8f';
-const authToken = 'a0b157791fbe4ad0b8412b2f56e0113b';
+const authToken = '0b338d4829a43d29ece4749c0aa9c7bc';
 const client = require('twilio')(accountSid, authToken);
 
 router.post('/send-otp', function (req, res) {
@@ -12,7 +12,7 @@ router.post('/send-otp', function (req, res) {
     client.messages
       .create({body: 'Your OTP is: '+otp, from: '+19389999952', to: req.body.mobile_number })
       .then(message => res.json({ success: true, message: 'OTP send successfully!', otp: otp }) )
-	  .catch(e => { res.json({ success: false, message: e.message }) });
+	  .catch(e => { res.json({ success: false, message: e.message, from: 'twilio api' }) });
 })
 
 module.exports = router;
